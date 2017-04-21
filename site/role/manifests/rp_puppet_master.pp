622bb875e75973ddcc458ed1e36bf25b80f93241
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-04-21 21:32:12 +1000

    set the hiera dir

--- site/role/manifests/rp_puppet_master.pp
+++ site/role/manifests/rp_puppet_master.pp
@@ -1,4 +1,7 @@
 class role::rp_puppet_master {
+  class { "r_profile::puppet::master::hiera":
+    hieradir => "/etc/puppetlabs/code/environments/%{environment}/modules/hieradata",
+  }
   include r_role::puppet::master_minimal
   include r_profile::virtual::vagrant
 }
